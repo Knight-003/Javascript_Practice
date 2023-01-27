83dba30b16580f7d0df610b52d4474eff6473509
na52m2002 <na52m2002@gmail.com>
2023-01-27 22:23:50 +0530

    functions (adding some more implimentation type)

--- functions.js
+++ functions.js
@@ -1,6 +1,6 @@
 "use strict "
 
-
+//this is function decleration we can use it anywhere in our program
 function add(a, b) {
 
 
@@ -17,3 +17,12 @@ console.log(add(1, 2));
 //this is also a type of function
 let num = Number('23');
 console.log(typeof (num));
+// in javascript functions are just a value therefore we can store it in a variable 
+//this is function expression and we cant use it before decleration
+
+let multiply2 = function (a, b) {
+
+    return (a * b);
+}
+
+console.log(multiply2(5, 2));
