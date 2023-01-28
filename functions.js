9ec14b95f93996b8e9871078109639a8c278bbe0
na52m2002 <na52m2002@gmail.com>
2023-01-29 02:07:19 +0530

    adding function of function code in file

--- functions.js
+++ functions.js
@@ -19,10 +19,29 @@ let num = Number('23');
 console.log(typeof (num));
 // in javascript functions are just a value therefore we can store it in a variable 
 //this is function expression and we cant use it before decleration
+//thi is also example of functions in function
+let multiply_of_add_div = function (a, b) {
+    let c = add(a, b);
+    return (c * (a / b));
 
-let multiply2 = function (a, b) {
-
-    return (a * b);
 }
 
-console.log(multiply2(5, 2));
+console.log(multiply_of_add_div(5, 2));
+
+//Aero functions
+const age = birthyear => 2037 - birthyear;
+
+console.log("the age is " + age(2001));
+
+const retAge = (birthyear) => {
+    const age = 2037 - birthyear;
+    const ret = 65 - age;
+    return (`the time  left for retirement is ${ret} years`);
+
+
+}
+
+
+
+console.log(retAge(2001));
+
